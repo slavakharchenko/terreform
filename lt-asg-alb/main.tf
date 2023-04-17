ce32d2528439a2e398ff3340e3aac77e090c067a
Viacheslav <viacheslav.kharchenko@pm.bet>
2023-04-18 01:02:09 +0300

    add vpc module

--- lt-asg-alb/main.tf
+++ lt-asg-alb/main.tf
@@ -1,19 +1,66 @@
-// Get default VPC
-data "aws_vpc" "default-vpc" {
-  default = "true"
-}
+module "training_terraform_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
 
-// Get all subnets id from default VPC for APL
-data "aws_subnets" "default-subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default-vpc.id]
+  name = "training_terraform_vpc"
+  cidr = "10.0.0.0/24"
+
+  azs             = ["eu-central-1a", "eu-central-1b"]
+  private_subnets = ["10.0.0.0/28", "10.0.0.16/28"]
+  public_subnets  = ["10.0.0.32/28", "10.0.0.48/28"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    project = "training_terraform"
   }
 }
 
-// Get security group from default VPC for APL
-data "aws_security_group" "default-security-group" {
-  vpc_id = data.aws_vpc.default-vpc.id
+resource "aws_security_group" "security_group_for_launch_template" {
+  vpc_id = module.training_terraform_vpc.vpc_id
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = module.training_terraform_vpc.public_subnets_cidr_blocks
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = module.training_terraform_vpc.public_subnets_cidr_blocks
+  }
+
+  tags = {
+    Name    = "Terraform_security_group_for_launch_template"
+    project = "training_terraform"
+  }
+}
+
+resource "aws_security_group" "security_group_for_alb" {
+  vpc_id = module.training_terraform_vpc.vpc_id
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "Terraform_security_group_for_alb"
+    project = "training_terraform"
+  }
 }
 
 resource "aws_launch_template" "terraform_launch_template" {
@@ -22,10 +69,7 @@ resource "aws_launch_template" "terraform_launch_template" {
 
   image_id      = "ami-08f54b258788948e1"
   instance_type = "t2.micro"
-
-  network_interfaces {
-    associate_public_ip_address = true
-  }
+  vpc_security_group_ids = [aws_security_group.security_group_for_launch_template.id]
 
   key_name  = "ec2"
   user_data = filebase64("webserver.sh")
@@ -50,7 +94,8 @@ resource "aws_lb_target_group" "terraform-target-group" {
   name     = "terraform-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default-vpc.id
+  vpc_id   = module.training_terraform_vpc.vpc_id
+
   health_check {
     path                = "/"
     port                = 80
@@ -70,10 +115,10 @@ resource "aws_lb" "terraform-application-lb" {
   name               = "terraform-application-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.default-security-group.id]
-  subnets            = data.aws_subnets.default-subnets.ids
+  security_groups    = [aws_security_group.security_group_for_alb.id]
+  subnets            = module.training_terraform_vpc.public_subnets
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     project = "training_terraform"
@@ -95,7 +140,7 @@ resource "aws_autoscaling_policy" "terraform_autoscaling_policy" {
 }
 
 resource "aws_autoscaling_group" "terraform_autoscaling_group" {
-  availability_zones = ["eu-central-1a", "eu-central-1b"]
+  vpc_zone_identifier = module.training_terraform_vpc.private_subnets
   desired_capacity   = 2
   max_size           = 3
   min_size           = 2
